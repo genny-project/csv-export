51ccc0c30a293d1d43c145102089cff99ad78525
Nischal Gautam <nischal1024@gmail.com>
2018-11-07 15:28:47 +1100

    test

--- src/controllers/csv.controller.js
+++ src/controllers/csv.controller.js
@@ -8,10 +8,10 @@ const csv = (req, res) => {
   // Default the request to be null to prevent inconsistencies
   const csvString = req.body || null;
   if (csvString) {
-    const strrr = '"CompanyName", "ReleaseNumber", "ReleaseDate", "RailYard", "ShippingCompany", "NumberOfContainer", "ContainerSize", "DeliveryDate", "AllocatedDriver", "ContactNumber", "ContainerId", "ContainerNumber",\n"Name","Number","2018-11-24","Yard","Line","1","45DR","2018-11-07T05:15:31","PER_SAFAL0079_AT_GMAILCOM","Number","1212","45DR",\n';
-
+    // const strrr = '"CompanyName", "ReleaseNumber", "ReleaseDate", "RailYard", "ShippingCompany", "NumberOfContainer", "ContainerSize", "DeliveryDate", "AllocatedDriver", "ContactNumber", "ContainerId", "ContainerNumber",\n"Name","Number","2018-11-24","Yard","Line","1","45DR","2018-11-07T05:15:31","PER_SAFAL0079_AT_GMAILCOM","Number","1212","45DR",\n';
+    const check = csvString;
     try {
-      const arr = CSV.parse(strrr);
+      const arr = CSV.parse(check);
       const filename = Date.now();
       const csvWriter = createCsvWriter({
         header: arr[0],
