b49c48b7264c6eb68857e838a8560c5119a783ab
Nischal Gautam <nischal1024@gmail.com>
2018-11-07 15:14:25 +1100

    error handling and filename

--- src/controllers/csv.controller.js
+++ src/controllers/csv.controller.js
@@ -1,38 +1,39 @@
 import * as CSV from 'csv-string';
-import fs from 'fs';
 import path from 'path';
+
 const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 
 
 const csv = (req, res) => {
+  // Default the request to be null to prevent inconsistencies
   const csvString = req.body || null;
-  const strrr  = '"CompanyName", "ReleaseNumber", "ReleaseDate", "RailYard", "ShippingCompany", "NumberOfContainer", "ContainerSize", "DeliveryDate", "AllocatedDriver", "ContactNumber", "ContainerId", "ContainerNumber",\n"Name","Number","2018-11-24","Yard","Line","1","45DR","2018-11-07T05:15:31","PER_SAFAL0079_AT_GMAILCOM","Number","1212","45DR",\n';
-  const arr = CSV.parse(strrr);
+  if (csvString) {
+    const strrr = '"CompanyName", "ReleaseNumber", "ReleaseDate", "RailYard", "ShippingCompany", "NumberOfContainer", "ContainerSize", "DeliveryDate", "AllocatedDriver", "ContactNumber", "ContainerId", "ContainerNumber",\n"Name","Number","2018-11-24","Yard","Line","1","45DR","2018-11-07T05:15:31","PER_SAFAL0079_AT_GMAILCOM","Number","1212","45DR",\n';
 
-  const csvWriter = createCsvWriter({
-    header: arr[0],
-    path: path.join(__dirname, '../../public/uploads/file.csv'),
-  });
+    try {
+      const arr = CSV.parse(strrr);
+      const filename = Date.now();
+      const csvWriter = createCsvWriter({
+        header: arr[0],
+        path: path.join(__dirname, `../../public/uploads/${filename}.csv`),
+      });
 
+      const records = arr;
 
-
+      csvWriter.writeRecords(records) // returns a promise
+        .then(() => {
+          console.log('...Done');
+          return res.status(200).json({ message: 'Done' });
+        });
+    } catch (err) {
+      res.status(500);
+      console.log(' Error While writing csv');
+      return res.json({ message: 'Error writing file ' });
+    }
+  } else {
+    return res.status(500).json({ message: "String not found or doesn't exists" });
+  }
 };
 
 
 export { csv };
-
-
-
-// function exportCSVFiles(req, res) {
-//   Job.find()
-//     .populate('company')
-//     .populate('jobs')
-//     .exec((err, company) => {
-//       if (err) return res.send(err);
-//       fs.writeFile(path.join(__dirname, '../public/file.csv'), company, (err) => {
-//         if (err) throw err;
-//         console.log('Docker test');
-//         return res.sendFile(path.join(__dirname, '../public/file.csv'));
-//       });
-//     });
-// }
