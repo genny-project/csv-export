af9af06511058ec9d3438722a9ccf289cff4c2c8
Nischal Gautam <nischal1024@gmail.com>
2018-11-27 16:45:17 +1100

    refactoring and better error handling

--- src/controllers/csv.controller.js
+++ src/controllers/csv.controller.js
@@ -2,18 +2,14 @@ import aws from 'aws-sdk';
 import * as CSV from 'csv-string';
 import path from 'path';
 import uuid from 'uuid/v1';
-import fs from 'fs';
-import csvToJson from 'csvtojson';
 
 import s3Upload from '../utils/aws.utils';
-
 import config from '../config';
 
 
 const BUCKET_NAME = config.bucketName;
 const IAM_USER_KEY = config.keyId;
 const IAM_USER_SECRET = config.accessKey;
-const URL = config.url;
 
 aws.config.update({ accessKeyId: IAM_USER_KEY, secretAccessKey: IAM_USER_SECRET });
 const s3bucket = new aws.S3({ params: { Bucket: BUCKET_NAME } });
@@ -24,38 +20,36 @@ const csv = (req, res) => {
   const { csvString } = req.body || null;
 
   console.log('request from the body', csvString, '\n \n');
-  if (csvString) {
-    const check = csvString;
+  if (csvString || csvString !== '' || csvString !== undefined || csvString !== null || csvString !== 'undefined') {
     try {
-      const parsedCSV = CSV.parse(check);
-      const jsonStringified = JSON.stringify(parsedCSV);
-
-      // console.log('###########');
-      // console.log(parsedCSV);
-      // console.log(jsonStringified);
-      // console.log('#####');
-
+      // get the  array out of the string supplieed
+      const parsedCSV = CSV.parse(csvString);
       const arr = parsedCSV;
+
+      // Generate a unique filename for every filename we are creating
       const filename = uuid();
 
+      // seperate the data into header and body
+      // the first row in an array is an header in csv
+      // rest of them are body part in CSV
+      const header = arr[0];
+      const body = arr.splice(1);
+
+      console.log(header);
+      console.log(body);
+
+      // write header data
       const csvWriter = createCsvWriter({
-        header: arr[0],
+        header,
         path: path.join(__dirname, `../../public/uploads/${filename}.csv`),
       });
 
-      const records = arr.splice(1);
+      // write the body data
 
-      // const arrFromRecors = Array.from(records);
-      // console.log('RRRY conversion', arrFromRecors);
-
-      /** **************** new stuff goes here **************************** */
-      /** ****************************************************************** */
-      console.log('data to be written as csv file', records);
-
-      csvWriter.writeRecords(records) // returns a promise
+      csvWriter.writeRecords(body) // returns a promise
         .then(() => {
           console.log('...File written on the disk');
-
+          console.log('s3 upload begins');
           /* s3 upload begins here */
           const dummyFile = {};
           dummyFile.originalFilename = filename;
@@ -77,9 +71,9 @@ const csv = (req, res) => {
           console.log(err);
         });
     } catch (err) {
+      console.log('Error While writing csv');
       console.log(err);
       res.status(500);
-      console.log(' Error While writing csv');
       return res.json({ message: 'Error writing file ' });
     }
   } else {
@@ -88,41 +82,44 @@ const csv = (req, res) => {
 };
 
 
+// method for downloading csv file from the filename provided
 const getCsv = (req, res) => {
   const { fileName } = req.params;
   console.log(fileName);
 
+  // configure bucket options
   const options = {
     Bucket: config.bucketName,
     Key: fileName,
   };
 
+  // find the data in s3
   s3bucket.getObject(options, (err, data) => {
     if (err) {
       console.log(err.stack);
-      return res.send({ message: 'Error while getting data from s3 bucket' });
+      return res.json({ message: 'Error while getting data from s3 bucket' });
     }
 
     // res.attachment(data.body);rs
     console.log('DATA FROM S# BUCKET DOWNLOAD', data);
-    if (data) {
-      console.log('DATA COMING FROM AWS');
-      console.log('FILENAME', fileName);
-      console.log('DATA', data);
-      const bufferData = data.Body;
-      return res.send(Buffer.from(bufferData));
+
+    // if existssend the data as an attachment
+    try {
+      if (data) {
+        console.log('DATA COMING FROM AWS');
+        console.log('FILENAME', fileName);
+        console.log('DATA', data);
+        const bufferData = data.Body;
+        return res.send(Buffer.from(bufferData)); // using new buffer API
+      }
+    } catch (err) {
+      console.log('No data received from s3 please try again');
+      return res.json({ message: ' No data received from s3' });
     }
+
+
     console.log('NO DATA');
   });
 };
 
 export { csv, getCsv };
-
-
-async function fsWrite(filename, data) {
-  fs.writeFile('../../public/uploads/filename1.csv', data, (err) => {
-    if (err) {
-      throw new Error(err);
-    }
-  });
-}
